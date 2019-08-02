198ebd8a2be4259bb248b617f4235deb6eea868a
jinchen <358964894@qq.com>
2019-08-02 17:38:54 +0800

    获取项目中的url

---
+++ src/main/java/com/eric/shirodemo/config/urlConfig.java
@@ -0,0 +1,36 @@
+package com.eric.shirodemo.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author JinChen
+ * @Title: urlConfig
+ * @Package com.eric.shirodemo.config
+ * @Description: TODO()
+ * @date 2019/8/2 17:36
+ * Copyright (c) 1994-2019 Scjydz.com All Rights Reserved.
+ */
+@Component
+public class urlConfig {
+    @Resource
+    private WebApplicationContext webApplicationContext;
+    public void getUrls(){
+        RequestMappingHandlerMapping mapping = webApplicationContext.getBean(RequestMappingHandlerMapping.class);
+        Map<RequestMappingInfo, HandlerMethod> map = mapping.getHandlerMethods();
+        //装载url信息结果集
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> m : map.entrySet()) {
+            RequestMappingInfo info = m.getKey();
+            PatternsRequestCondition p = info.getPatternsCondition();
+        }
+    }
+}
